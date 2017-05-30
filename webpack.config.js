87ddbf8a3944999373b88e79f7f4056312d8f49b
Matthew Hersh <mattmhersh@gmail.com>
2017-05-30 17:50:46 +0000

    Moved files. Updated config

--- webpack.config.js
+++ webpack.config.js
@@ -1,7 +1,15 @@
+var path = require("path");
+
 module.exports = {
+    context: path.resolve('js'),
     entry: ["./utils.js", "./app.js"],
     output: {
+        path: path.resolve('build/js/'),
+        publicPath: '/public/assets/js/',
         filename: "bundle.js"
+    },
+    devServer: {
+        contentBase: 'public'
     },
     watch: false,
     module: {
